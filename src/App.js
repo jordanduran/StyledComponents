1f49cf7485dc72922e30928f1a57da0113ece99f
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2022-01-27 14:44:09 -0500

    Seperate styled components into own files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import styled from 'styled-components';
+import BasicTitle from './components/BasicTitle';
+import { DefaultButton } from './components/Buttons';
 
-const BasicTitle = styled.h1`
-  text-align: center;
-  text-transform: capitalize;
-`;
-
-const BasicButton = styled.button`
-  background-color: slateblue;
-  color: white;
-  border-style: none;
-  padding: 5px 30px;
-  border-radius: 5px;
-  text-transform: capitalize;
-  cursor: pointer;
-  display: block;
-  margin: 2em auto;
-`;
+// import styled from 'styled-components';
 
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
       <BasicTitle>styled component</BasicTitle>
-      <BasicButton>click me</BasicButton>
+      <DefaultButton>click me</DefaultButton>
     </div>
   );
 }
---
+++ src/components/Buttons.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const DefaultButton = styled.button`
+  background-color: slateblue;
+  color: white;
+  border-style: none;
+  padding: 8px 60px;
+  border-radius: 5px;
+  text-transform: capitalize;
+  cursor: pointer;
+  display: block;
+  margin: 2em auto;
+`;

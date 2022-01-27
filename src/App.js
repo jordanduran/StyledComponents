00127fe0fce3f90b78e42d5b6d4ba9135ea0e1d2
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2022-01-27 14:49:49 -0500

    Passing simple props

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { DefaultButton } from './components/Buttons';
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
+      <BasicTitle specialColor>styled component</BasicTitle>
       <BasicTitle>styled component</BasicTitle>
       <DefaultButton>click me</DefaultButton>
     </div>
